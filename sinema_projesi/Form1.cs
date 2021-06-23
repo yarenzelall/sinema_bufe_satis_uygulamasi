d0821a927adfdd6cca0d8ea6d37af204354cf0f7
Yaren Zelal TOSU <yarentosu@gmail.com>
2021-06-23 09:18:18 +0300

    Add project files.

---
+++ sinema_projesi/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sinema_projesi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int kasatutar = 0;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int misir, su, cay, bilet,toplam;
+            misir = Convert.ToInt16(txtmisir.Text);
+            su = Convert.ToInt16(txtsu.Text);
+            cay = Convert.ToInt16(txtcay.Text);
+            bilet = Convert.ToInt16(txtbilet.Text);
+
+            misir = misir * 4;
+            su = su * 1;
+            cay = cay * 2;
+            bilet = bilet * 8;
+
+            toplam = (misir + su + cay + bilet);
+            lbltoplam.Text = toplam.ToString();
+            kasatutar = toplam + kasatutar;
+            lblkasa.Text = kasatutar.ToString();
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            txtmisir.Text = "";
+            txtsu.Text = "";
+            txtcay.Text = "";
+            txtbilet.Text = "";
+            txtmisir.Focus();
+            lbltoplam.Text = "00 TL";
+        }
+    }
+}
